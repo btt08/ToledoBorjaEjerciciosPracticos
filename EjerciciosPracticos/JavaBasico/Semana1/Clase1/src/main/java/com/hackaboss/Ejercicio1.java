73751c081036388958dbffa90019c68985e5a8cb
btt08 <borjattrapiella@gmail.com>
2023-12-13 17:06:05 +0100

    Bugfixes

--- EjerciciosPracticos/JavaBasico/Semana1/Clase1/src/main/java/com/hackaboss/Ejercicio1.java
+++ EjerciciosPracticos/JavaBasico/Semana1/Clase1/src/main/java/com/hackaboss/Ejercicio1.java
@@ -20,5 +20,6 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         String name = "Borja";
         System.out.println("Hola " + name + ", bienvenido al sistema");
+        
     }
 }
